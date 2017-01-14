deba33c5eb62d428ef14fe4ada09284d725aa56f
Ikarago <ikarago@outlook.com>
2017-01-14 23:17:08 +0100

    PoC working

--- ProjectRome/ProjectRome/App.xaml.cs
+++ ProjectRome/ProjectRome/App.xaml.cs
@@ -73,7 +73,7 @@ namespace ProjectRome
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                    rootFrame.Navigate(typeof(Views.TempPage), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
@@ -119,7 +119,7 @@ namespace ProjectRome
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(MainPage),comments);
+                    rootFrame.Navigate(typeof(Views.TempPage),comments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
