150696a355b99905346be1e2b3cf068dbd73c980
Rick D <ikarago@outlook.com>
2017-03-19 11:30:38 +0100

    Changes to the MainPage

--- ProjectRome/ProjectRome/Views/MainPage.xaml.cs
+++ ProjectRome/ProjectRome/Views/MainPage.xaml.cs
@@ -13,18 +13,19 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
-
 namespace ProjectRome.Views
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
             this.InitializeComponent();
+            sbWarpBackgroundAnimation.Begin();
+        }
+
+        private void btnLink_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
